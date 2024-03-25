0c54e97c68cf82550479b3ad34d24b11854a6187
David Kissinger <davidgkissinger@gmail.com>
2024-03-24 18:45:33 -0700

    Create siteorigin-various-functions.php

---
+++ siteorigin-various-functions.php
@@ -0,0 +1,8 @@
+<?php
+
+/*
+
+This file contains various random functions, hooks, and stuff useful on WordPress sites built on the SiteOrigin framework.
+These may make reference to specific SiteOrigin functions so they won't work on other sites.
+
+*/
