c013d1630dc66a74f2b12967b0cd7b010fe7f2fc
David Kissinger <davidgkissinger@gmail.com>
2024-03-24 19:14:02 -0700

    Update siteorigin-various-functions.php

--- siteorigin-various-functions.php
+++ siteorigin-various-functions.php
@@ -6,3 +6,20 @@ This file contains various random functions, hooks, and stuff useful on WordPres
 These may make reference to specific SiteOrigin functions so they won't work on other sites.
 
 */
+
+// This function makes a shortcode to put the feature image anywhere on the page. Good for SO's dynamic CPT templates
+// It only works inside the Loop i.e. on a post, page or CPR
+// Makes use of SO function siteorigin_corp_entry_thumbnail
+// Come to think of it, in this writing it doesn't use the SO function after all because it acts like an action
+// hook and doesn't return anything. Also it generates a big mess of code more appropriate for an actual blog post header
+
+function dk_get_entry_thumbnail( $atts ) {
+  // attribute defaults to title so that is returned it no atts are given
+  global $post;
+  $a = shortcode_atts( array(
+		'imagesize' => 'medium',
+	 ), $atts );
+  $alt = esc_html( get_the_title() );
+  return wp_get_attachment_image( get_the_ID(), $a['imagesize'], false, array( 'alt' => $alt )  );
+}
+add_shortcode( 'getfeatureimage', 'dk_get_entry_thumbnail' );
